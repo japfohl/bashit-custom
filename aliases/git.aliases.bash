4d7650313950bb4aaa60b975e52787d24d8c335c
Joe Pfohl <joepfohl@gmail.com>
2017-10-30 10:39:31 -0400

    add git aliases

---
+++ aliases/git.aliases.bash
@@ -0,0 +1,9 @@
+# custom engironment variables for git
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gcm='git commit -m'
+alias gs='git status'
+alias gd='git diff'
+alias ga='git add'
+alias git-cleanup='git branch --merged | egrep -v "(^\*|master|dev)" | xargs git branch -d'
+alias gb='git branch'
