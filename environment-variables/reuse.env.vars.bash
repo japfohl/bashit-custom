829850241cdd52588465b42dbe120575e7f032a2
Joe Pfohl <joepfohl@gmail.com>
2017-10-30 10:06:26 -0400

    create custom environment variables for reuse project

---
+++ environment-variables/reuse.env.vars.bash
@@ -0,0 +1,7 @@
+# Custom environment variables for CS467 Capstone Project
+export REUSE_DB_USER="root"
+export REUSE_DB_PW="root"
+export REUSE_DB_URL="127.0.0.1"
+export REUSE_DB_NAME="reuse_db"
+export API_ADDR="127.0.0.1:8001"
+export BING_API_KEY="AlKcRwWYnXjyBcUrW1rcP1QqDC7ZjH9SCVqVHMY1d8rk0LjmHjol80Rgu8qqsJ-P"
